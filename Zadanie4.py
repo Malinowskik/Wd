9a001074c62e3ac566136f0faf172da3ef7e6141
Malinowskik <74622359+Malinowskik@users.noreply.github.com>
2021-03-26 20:29:55 +0000

    Add files via upload

---
+++ Zadanie4.py
@@ -0,0 +1,58 @@
+# Zadanie 1
+import numpy as np
+print("Zadanie 1")
+a = np.linspace(3, 45, 15) # poczatek 3, końcowa liczba 45, liczba powtórzeń
+print(a)
+print('\n')
+
+# Zadanie 2
+print("Zadenie 2")
+b = np.arange(10, dtype='float') # tworzenie listy float
+c = b.astype('int') # przypisanie wartości z listy 1 do 2 i zmiana na int
+print(b)
+print(b.dtype) # sprawdzenie typu danych
+print(c)
+print(c.dtype) #sprawdzenie typu danych
+print('\n')
+
+# Zadanie 3
+'''print("Zadanie 3")
+n = input("Wpisz liczbę całkowitą: ")
+n = int(n)
+d = np.arange(1, n*n+1)
+print(d)
+print('\n')
+
+# Zadanie 4
+print("Zadanie 4")
+e = input("Podaj 1 parametr: ")
+f = input("Podaj 2 parametr: ")
+e = int(e)
+f = int(f)
+def g(p, l):
+    k = np.logspace(1, l, num=l, base=p)
+    return k
+
+print(g(e,f))
+print('\n')
+
+# Zadanie 5
+print("Zadanie 5")
+g = input("Podaj długość wektora: ")
+g = int(g)
+def generate_vector(i):
+    w = [x for x in range(i+1)]
+    w = w[::-1]
+    y = np.diag(w)
+    return y
+print((generate_vector(g)))
+print('\n') '''
+
+#Zadanie 6
+print("Zadanie 6")
+
+c = 'krzysiu'
+v = 'krawczyk'
+b = 'kajak'
+
+# Brak czasu na wykonanie pozostałych zadań. Pracuje po ok 12h dziennie
